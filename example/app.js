5c5488040bd421932ff2b81446235598e22de2ff
Cory Smith <cory.m.smith@gmail.com>
2016-02-27 16:32:35 -0700

    Adding example app

---
+++ example/app.js
@@ -0,0 +1,44 @@
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const socketio = require('feathers-socketio');
+const bodyParser = require('body-parser');
+const errorHandler = require('feathers-errors/handler');
+const mailgunService = require('../lib').default;
+
+// Create a feathers instance.
+var app = feathers()
+// Enable REST services
+  .configure(rest())
+  // Enable Socket.io services
+  .configure(socketio())
+  // Turn on JSON parser for REST services
+  .use(bodyParser.json())
+  // Turn on URL-encoded parser for REST services
+  .use(bodyParser.urlencoded({extended: true}));
+
+app.use('/mailer', mailgunService({
+    apiKey: 'API_KEY',
+    domain: 'DOMAIN' // ex. your.domain.com
+  }
+));
+
+// Send an email!
+app.service('mailer').create({
+  from: 'cory.m.smith@gmail.com',
+  to: 'cory.m.smith@gmail.com',
+  subject: 'Mailgun test',
+  html: 'Email body'
+}).then(function (result) {
+  console.log('Sent email', result);
+}).catch(err => {
+  console.log(err);
+});
+
+app.use(errorHandler());
+
+// Start the server.
+const port = 3030;
+
+app.listen(port, function () {
+  console.log(`Feathers server listening on port ${port}`);
+});
