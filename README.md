a47c41369d3c7fdc2458ba3a27396a196dcf5c26
Cory Smith <cory.m.smith@gmail.com>
2016-02-27 17:42:00 -0700

    Adding a few more tests to ensure data is sent correctly to Mailgun.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 > A [Mailgun](https://www.mailgun.com) Service for [FeatherJS](https://github.com/feathersjs).
 
+## TODO
+- [ ] Show how to not expose externally, maybe even by default for security reasons
+ 
 
 ## Installation
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,13 @@
 /*jshint expr: true*/
 
+import chai from 'chai';
 import { expect } from 'chai';
 import assert from 'assert';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
 import feathers from 'feathers';
 
 import server from './test-app';
@@ -10,6 +16,20 @@ import service from '../src';
 
 const mailgun = service({apiKey: 'API_KEY', domain: 'mail.feathersjs.com'});
 const app = feathers().use('/mailer', mailgun);
+
+const validParams = {
+  from: 'from@from.com',
+  to: 'to@to.com',
+  subject: 'Email Subject',
+  html: 'message html'
+};
+
+const validParamsWithArrayInToField = {
+  from: 'from@from.com',
+  to: ['to@to.com', 'to2@to.com'],
+  subject: 'Email Subject',
+  html: 'message html'
+};
 
 describe('Mailgun Service', function () {
 
@@ -30,7 +50,7 @@ describe('Mailgun Service', function () {
     });
   });
 
-  describe('Sending messages', () => {
+  describe('Validation', () => {
 
     describe('when missing from field', () => {
       it('throws an error', (done) => {
@@ -76,4 +96,60 @@ describe('Mailgun Service', function () {
       });
     });
   });
+
+  var mailgunSend;
+
+  describe('Sending messages', () => {
+
+    before(function (done) {
+      mailgunSend =
+      sinon
+        .stub(app.service('mailer'), '_send', function (data, callback) {
+          callback(null, {success: true});
+        });
+      done();
+    });
+
+    after(function (done) {
+      mailgunSend.restore();
+      done();
+    });
+
+    describe('when sending to an array of email addresses', () => {
+
+      it('correctly parses into comma delimited string', (done) => {
+
+        var expectedParams = {
+          from: validParamsWithArrayInToField.from,
+          to: 'to@to.com,to2@to.com',
+          subject: validParamsWithArrayInToField.subject,
+          html: validParamsWithArrayInToField.html
+        };
+        app.service('mailer').create(validParamsWithArrayInToField).then(result => {
+          expect(result.success).to.eql(true);
+          expect(mailgunSend).to.have.been.calledWith(expectedParams);
+          done();
+        });
+      });
+    });
+
+    describe('when all fields are valid', () => {
+      it('successfully sends a message', (done) => {
+        app.service('mailer').create(validParams).then(result => {
+          expect(result.success).to.eql(true);
+          expect(mailgunSend).to.have.been.calledWith(validParams);
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('Common functionality', () => {
+    it('is CommonJS compatible', () => {
+      console.log(typeof require('../lib'));
+      assert.ok(typeof require('../lib') === 'function');
+    });
+  });
+
 });
